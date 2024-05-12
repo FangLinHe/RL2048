aef339338d35a3812ab572629a6905fc6258ea10
Fang-Lin He <fanglin.he.ms@gmail.com>
2024-05-12 11:29:58 +0200

    Remove dropout

--- RL2048/DQN/net.py
+++ RL2048/DQN/net.py
@@ -22,7 +22,6 @@ class Net(nn.Module):
             layers.append(nn.Linear(in_features, out_features, bias=bias))
             if activation_layer is not None:
                 layers.append(activation_layer())
-            layers.append(nn.Dropout())
             in_features = out_features
         layers.append(nn.Linear(in_features, output_feature_size, bias))
 
