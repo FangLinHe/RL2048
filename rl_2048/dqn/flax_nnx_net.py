78c2580387d01e378aef6c05b6103be1ce8d6c3e
Fang-Lin He <fanglin.he.ms@gmail.com>
2024-06-24 13:02:50 +0200

    [WIP] Add flax nnx implementation of Net

--- rl_2048/dqn/flax_nnx_net.py
+++ rl_2048/dqn/flax_nnx_net.py
@@ -11,10 +11,12 @@ class PolicyNet(Protocol):
     def load(self, model_path: str): ...
 """
 
-from typing import Callable
+from typing import Callable, Union
 
 from flax import nnx
 from jaxtyping import Array
+
+from rl_2048.dqn.common import PREDEFINED_NETWORKS
 
 
 class ResidualBlock(nnx.Module):
@@ -60,3 +62,84 @@ class ResidualBlock(nnx.Module):
             )[:, :, 0]
 
         return x + residual
+
+
+class Net(nnx.Module):
+    def __init__(
+        self,
+        in_dim: int,
+        hidden_dims: tuple[int, ...],
+        output_dim: int,
+        net_activation_fn: Callable,
+        residual_mid_dims: tuple[int, ...],
+        rngs: nnx.Rngs,
+    ):
+        if len(residual_mid_dims) == 0:
+            residual_mid_dims = tuple(0 for _ in range(len(hidden_dims)))
+
+        def validate_args():
+            N_hidden, N_res = len(hidden_dims), len(residual_mid_dims)
+            if N_hidden != N_res:
+                raise ValueError(
+                    "`residual_mid_dims` should be either empty or have the same "
+                    f"length as `hidden_dims` ({N_hidden}), but got ({N_res})"
+                )
+
+        validate_args()
+
+        layers: list[nnx.Module] = []
+        for residual_mid_dim, hidden_dim in zip(residual_mid_dims, hidden_dims):
+            block: list[Union[nnx.Module, Callable]] = []
+            if residual_mid_dim == 0:
+                block.append(nnx.Linear(in_dim, hidden_dim, use_bias=False, rngs=rngs))
+                block.append(nnx.BatchNorm(hidden_dim, rngs=rngs))
+            else:
+                block.append(
+                    ResidualBlock(
+                        in_dim, residual_mid_dim, hidden_dim, net_activation_fn, rngs
+                    )
+                )
+            in_dim = hidden_dim
+            block.append(net_activation_fn)
+            layers.append(nnx.Sequential(*block))
+
+        layers.append(nnx.Linear(in_dim, output_dim, rngs=rngs))
+
+        self.layers = nnx.Sequential(*layers)
+
+    def __call__(self, x: Array):
+        return self.layers(x)
+
+
+def _load_predefined_net(
+    network_version: str, in_dim: int, output_dim: int, rngs: nnx.Rngs
+) -> Net:
+    if network_version not in PREDEFINED_NETWORKS:
+        raise NameError(
+            f"Network version {network_version} not in {PREDEFINED_NETWORKS}."
+        )
+
+    hidden_layers: tuple[int, ...]
+    residual_mid_feature_sizes: tuple[int, ...]
+    if network_version == "layers_1024_512_256":
+        hidden_layers = (1024, 512, 256)
+        residual_mid_feature_sizes = ()
+    elif network_version == "layers_512_512_residual_0_128":
+        hidden_layers = (512, 512)
+        residual_mid_feature_sizes = (0, 128)
+    elif network_version == "layers_512_256_128_residual_0_64_32":
+        hidden_layers = (512, 256, 128)
+        residual_mid_feature_sizes = (0, 64, 32)
+    elif network_version == "layers_512_256_256_residual_0_128_128":
+        hidden_layers = (512, 256, 256)
+        residual_mid_feature_sizes = (0, 128, 128)
+
+    policy_net: Net = Net(
+        in_dim,
+        hidden_layers,
+        output_dim,
+        nnx.relu,
+        residual_mid_feature_sizes,
+        rngs,
+    )
+    return policy_net
--- tests/dqn/test_flax_nnx_net.py
+++ tests/dqn/test_flax_nnx_net.py
@@ -1,18 +1,59 @@
 import jax.numpy as jnp
 import pytest
 from flax import nnx
+from jax import Array
 
-from rl_2048.dqn.flax_nnx_net import ResidualBlock
+from rl_2048.dqn.common import PREDEFINED_NETWORKS
+from rl_2048.dqn.flax_nnx_net import Net, ResidualBlock, _load_predefined_net
 
 
-def test_residual_block():
-    rngs = nnx.Rngs(params=0)
-    x = jnp.ones((1, 4))
+@pytest.fixture
+def rngs() -> nnx.Rngs:
+    return nnx.Rngs(params=0)
+
+
+@pytest.fixture
+def input_array(batch: int = 1, dim: int = 4) -> Array:
+    return jnp.ones((batch, dim))
+
+
+@pytest.fixture
+def output_dim() -> int:
+    return 4
+
+
+def test_residual_block(rngs: nnx.Rngs, input_array: Array):
     for mid in (2, 4, 6):
-        ResidualBlock(4, mid, 4, nnx.relu, rngs)(x)
-        ResidualBlock(4, mid, 2, nnx.relu, rngs)(x)
+        ResidualBlock(4, mid, 4, nnx.relu, rngs)(input_array)
+        ResidualBlock(4, mid, 2, nnx.relu, rngs)(input_array)
 
     # out_dim must be smaller or equal to in_dim
     for mid in (2, 4, 6):
         with pytest.raises(TypeError):
-            ResidualBlock(4, mid, 8, nnx.relu, rngs)(x)
+            ResidualBlock(4, mid, 8, nnx.relu, rngs)(input_array)
+
+
+def test_predefined_nets(rngs: nnx.Rngs, input_array: Array, output_dim: int):
+    rngs = nnx.Rngs(params=0)
+
+    for network_version in PREDEFINED_NETWORKS:
+        _load_predefined_net(network_version, input_array.shape[1], output_dim, rngs)(
+            input_array
+        )
+
+
+def test_invalid_nets(rngs: nnx.Rngs, input_array: Array, output_dim: int):
+    input_dim: int = input_array.shape[1]
+
+    with pytest.raises(ValueError):
+        Net(
+            input_dim,
+            (2, 2, 2),
+            output_dim,
+            nnx.relu,
+            (2, 2),
+            rngs,
+        )
+
+    with pytest.raises(NameError):
+        _load_predefined_net("foo", input_dim, output_dim, rngs)
